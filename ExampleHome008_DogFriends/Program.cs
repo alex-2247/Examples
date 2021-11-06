04a98683b637e70c6856e841025e87d1ac1b5cb2
alex-2247 <alex2247@mail.ru>
2021-11-06 17:27:04 +0500

    add Example Home 008 DogFrends

---
+++ ExampleHome008_DogFriends/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int distance = 10000;
+int frend1speed = 1;
+int frend2speed = 2;
+int dogSpeed = 5;
+int friendNumber = 2;
+int currentDogRunTime;
+
+while (distance > 10)
+{
+    if (friendNumber == 1)
+    {
+        currentDogRunTime = distance / (frend1speed + dogSpeed);
+        friendNumber = 2;
+    }
+    else
+    {
+        currentDogRunTime = distance / (frend2speed + dogSpeed);
+        friendNumber = 1;
+    }
+    distance = distance - (frend1speed + frend2speed) * currentDogRunTime;
+    count = count + 1;
+    Console.Write(", ");
+    Console.Write(distance);
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз");
